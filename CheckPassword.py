23a694cff4bd12fb01110480bddb8b71f8ec4f23
Flakoran <Flakotaflak@gmail.com>
2026-04-23 08:10:48 +0200

    Enhance password check to include length validation
    Added length requirement for password validation.

--- CheckPassword.py
+++ CheckPassword.py
@@ -1,4 +1,5 @@
 # sem doplnte link na svoj kod
+# https://github.com/shalamun/SEN_pisomka_1
 
 import os;
 
@@ -6,16 +7,19 @@ def check_password(password: str) -> bool:
     small_letter = False
     capital_letter = False
     number = False
+    length = False
 
     for c in password:
         if(c.isdigit()):
-            number = True
-        elif(c.islower()):
             small_letter = True
         elif(c.isupper()):
             capital_letter = True
-    
+
+    if len(password.strip()) >= 8:
+        length = True
+
     return small_letter and capital_letter and number
+    return small_letter and capital_letter and number and length
 
 
 dir = os.path.dirname(__file__)                     # Nacita cestu k suboru
@@ -23,3 +27,4 @@ with open(dir + "/passwords.txt", 'r') as file:     # Otvori subor s heslami
     for password in file:                           # Prejde vsetky riadky/hesla
         if check_password(password):                # Skontroluje ich
             print(password, end = "")
+#ach jaj aaa
